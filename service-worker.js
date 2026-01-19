4d76057dd78809ac305405cdef824470ec6abbe8
lapin-studio <mikukojima0318@gmail.com>
2026-01-19 19:12:17 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "keeph-mini-v0.9";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+      return fetch(req)
+        .then((res) => {
+          // 同一オリジンのGETだけキャッシュ
+          if (req.method === "GET" && new URL(req.url).origin === self.location.origin) {
+            const clone = res.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(req, clone));
+          }
+          return res;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
